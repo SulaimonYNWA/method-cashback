8e9036704de1d8cf1f72db4486c0779998ba5264
Sulaimon <sulajmoninomov@gmail.com>
2020-11-03 13:49:20 +0500

    updeted purchases.go code

--- purchases.go
+++ purchases.go
@@ -6,32 +6,24 @@ type Purchase struct{
 	array []int64
 	cashback int64
 	sumcashback int64
+	sum int64
 }
 
-func(percent Purchase) countCashback(cashback int64, sum int64){
-	 percent.sumcashback = cashback*sum/100
+	func(percent Purchase) countCashback(){
+		percent.sum = int64(0)
+		for _, value := range percent.array{
+			percent.sum += value
+		}
+	percent.sumcashback = percent.cashback * percent.sum / 100
 	 fmt.Println(percent.sumcashback)
 }
 
 func main() {
-	
 	purchases := Purchase{
-		// array: []int64{100, 50, 70},
-		// cashback: 15,
+		array: []int64{100, 50, 70},
+		cashback: 15,
+		sumcashback: 0,
+		sum: 0,
 	}
-	purchases.array = []int64{100, 50, 70}
-	purchases.cashback = 15
-	purchases.sumcashback = 0
-
-	var sum int64 = 0
-	for _, value := range purchases.array{
-		sum += value
-	}
-
-fmt.Println(sum)
-finalSum:=sum*(100-purchases.cashback)/100
-fmt.Println(finalSum)
-
-purchases.countCashback(purchases.cashback, sum)
-
+purchases.countCashback()
 }
